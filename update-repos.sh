cc3393a0c2726cf5f3b975f44676ac201005e94b
Brandon Autrey <brandon.c.autrey@gmail.com>
2025-08-27 11:43:49 +0300

    Show summary of pulled changes
    Includes color coding of print statements

--- update-repos.sh
+++ update-repos.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+# Color definitions
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+GRAY='\033[0;90m'
+NC='\033[0m' # No Color
+
 SALEMOVE=~/salemove
 
 for dir in $SALEMOVE/*
@@ -5,7 +17,7 @@ do
   isGitRepo=$(git -C ${dir} rev-parse 2>/dev/null; echo $?)
   if [[ $isGitRepo != 0 ]]
   then
-    echo "Ignore $repo"
+    echo -e "${GRAY}Ignore $repo${NC}"
     continue
   fi
 
@@ -16,15 +28,43 @@ do
   # updatedAt=$(date -d ${status} | jq -r '.updatedAt')
   branch=$(git -C ${dir} symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
-  if [[ $branch != 'master' || $branch != 'main']]
+  if [ "$branch" != 'master' ] && [ "$branch" != 'main' ]
   then
-    echo "Repo \"$repo\" current branch: \"$branch\""
+    echo -e "${YELLOW}Repo \"$repo\" current branch: \"$branch\"${NC}"
   else
-    echo "Updating repo \"$repo\" master branch..."
-    git -C ${dir} pull
-    echo "Done updating"
+    echo -e "${BLUE}Updating repo \"$repo\" on branch \"$branch\"...${NC}"
+
+    # Get the current commit hash before pulling
+    before_commit=$(git -C ${dir} rev-parse HEAD)
+
+    # Perform the pull
+    pull_output=$(git -C ${dir} pull 2>&1)
+    pull_status=$?
+
+    if [ $pull_status -eq 0 ]; then
+      # Get the current commit hash after pulling
+      after_commit=$(git -C ${dir} rev-parse HEAD)
+
+      if [ "$before_commit" = "$after_commit" ]; then
+        echo -e "${GRAY}Already up to date${NC}"
+      else
+        # Count the number of new commits
+        commit_count=$(git -C ${dir} rev-list --count ${before_commit}..${after_commit})
+
+        # Get a brief summary of changes
+        if [ $commit_count -gt 0 ]; then
+          echo -e "${GREEN}Updated: $commit_count new commit(s)${NC}"
+          # Show the latest commit message
+          latest_commit=$(git -C ${dir} log -1 --pretty=format:"  Latest: %s (%an)")
+          echo -e "${CYAN}$latest_commit${NC}"
+        fi
+      fi
+    else
+      echo -e "${RED}Pull failed: $pull_output${NC}"
+    fi
+
+    echo -e "${PURPLE}Done updating${NC}"
 
     # todo: test is master is up to date, then do not update.
-    # todo: provide shortened summary of update, like number of commits.
   fi
 done
